23af7f9c4a74f9093a0fedf0966b74480e9cf6a7
Aftab <therealtopper@gmail.com>
2022-09-16 10:54:00 +0530

    first assignment completed!

---
+++ assignment1.py
@@ -0,0 +1,34 @@
+# 1. Write a command to get the Python version you are using.
+
+# ans. python --version on cmd
+
+# 2. Write a python script to print Hello python on the screen directly in the command line.
+print("Hello python")
+
+# 3. Write a python script and store it in a file and execute the file from the command line.
+a = 5;
+print(a)
+
+# 5. Write a python script to print hello on the first line and python on the second line.
+print("Hello")
+print("Python")
+
+# 6. Write a python script to print "MySirG" on the screen. 
+print("MySirG")
+print("Teacher's Day")
+
+# 8. Write a python script to print the value of a variable. Variable containsyour name as data.
+your_name = "MD AFTAB ALI"
+print(your_name)
+
+# 9. Write a python script to create variables to store your name, age, qualification, years of experience and print it.
+
+name, age, qualification, years_of_experience = "Md Aftab Ali", 20, "Full stack Development", 0;
+print(name, age, qualification,years_of_experience, sep="\n")
+
+# 10. Write a python script to print values of three variables, each in a new line. Variable contains integer values.
+# var1 = 10;
+# var2 = 11;
+# var3 = 12;
+var1,var2,var3 = 10,11,12;
+print(var1,var2,var3, sep=" ")
